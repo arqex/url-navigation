4ab884e5ba3e07347524a95419f0c69e97e29b50
Javier Marquez <javi@arqex.com>
2019-01-18 19:56:22 +0100

    Updates urlstack dependency

--- packages/react-urlstack/react-urlstack-playground/babel.config.js
+++ packages/react-urlstack/react-urlstack-playground/babel.config.js
@@ -1,14 +1,19 @@
+let Path = require('path');
+
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(false);
+
   return {
     presets: ['babel-preset-expo'],
+    /*
     plugins: [[
       'module-resolver', {
-        root: ['..'],
+        root: [ Path.join( __dirname, '..') ],
         alias: {
-          'react-urlstack': './react-urlstack.js'
+          // 'react-urlstack': './react-urlstack.js'
         }
       }
     ]]
+    */
   };
 };
